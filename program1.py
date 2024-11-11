713cf85480a55a565cdad35faaf2542f15b164b7
charanyaredddy <charanyareddy1904@gmail.com>
2024-11-11 14:11:07 +0530

    changes done

--- program1.py
+++ program1.py
@@ -1,6 +1,21 @@
 class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+        if not grid:
+            return 0
+        rows,cols=len(grid),len(grid[0])
+        island_count=0
+        def dfs(r,c):
+            if r<0 or r >=rows or c<0 or c>=cols or grid[r][c]=='w':
+                return
+            grid[r][c]='w'
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=='L':
+                    island_count+=1
+                    dfs(r,c)
+        return island_count
--- program2.py
+++ program2.py
@@ -1,5 +1,19 @@
-def decode_message( s: str, p: str) -> bool:
-
-# write your code here
-  
+def decode_message(s: str, p: str) -> bool:
+    memo = {}
+    def dp(i, j):
+        if (i, j) in memo:
+            return memo[(i, j)]
+        if i == len(s) and j == len(p):
+            return True
+        if j == len(p):
+            return False
+        match = (i < len(s) and (p[j] == s[i] or p[j] == '?'))
+        if p[j] == '*':
+            memo[(i, j)] = dp(i, j + 1) or (i < len(s) and dp(i + 1, j))
+            return memo[(i, j)]
+        if match:
+            memo[(i, j)] = dp(i + 1, j + 1)
+            return memo[(i, j)]
+        memo[(i, j)] = False
         return False
+    return dp(0, 0)
