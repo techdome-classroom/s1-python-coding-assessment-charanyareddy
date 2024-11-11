210f1387720ff42c3ac0757386f1eca52ad53007
charanyaredddy <charanyareddy1904@gmail.com>
2024-11-11 14:21:56 +0530

    changes done

--- program1.py
+++ program1.py
@@ -1,24 +1,24 @@
 class Solution:
-    def countIslands(self, map_grid: list[list[str]]) -> int:
-
-        def explore(x, y):
-            if x < 0 or y < 0 or x >= len(map_grid) or y >= len(map_grid[0]) or map_grid[x][y] != 'L':
+    def getTotalIsles(self, island_map: list[list[str]]) -> int:
+        
+        def explore_land(x, y):
+            if x < 0 or y < 0 or x >= len(island_map) or y >= len(island_map[0]) or island_map[x][y] != 'L':
                 return
-            map_grid[x][y] = 'W'
-            explore(x + 1, y)
-            explore(x - 1, y)
-            explore(x, y + 1)
-            explore(x, y - 1)
+            island_map[x][y] = 'W'
+            explore_land(x + 1, y)
+            explore_land(x - 1, y)
+            explore_land(x, y + 1)
+            explore_land(x, y - 1)
 
-        if not map_grid:
+        if not island_map:
             return 0
-
+        
         island_count = 0
 
-        for row in range(len(map_grid)):
-            for col in range(len(map_grid[0])):
-                if map_grid[row][col] == 'L':
-                    explore(row, col)
+        for row in range(len(island_map)):
+            for col in range(len(island_map[0])):
+                if island_map[row][col] == 'L':
+                    explore_land(row, col)
                     island_count += 1
-
+        
         return island_count
